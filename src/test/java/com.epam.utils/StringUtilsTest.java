2f27f89e919f3a1491172b27262a93b95a85aa73
lecras13 <lecras@tut.by>
2021-03-14 16:42:04 +0300

    second

---
+++ src/test/java/com.epam.utils/StringUtilsTest.java
@@ -0,0 +1,22 @@
+package com.epam.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringUtilsTest {
+    @Test
+    public void testIsPositiveNumberGiveTrue() {
+        boolean actual = StringUtils.isPositiveNumber("5");
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testIsPositiveNumberGiveFalse() {
+        boolean actual = StringUtils.isPositiveNumber("-5");
+
+        assertFalse(actual);
+    }
+}
